9f655b3cd3e8bf4818f9cfc6ca9d5465f8a8c514
Suriya <ktaro112@gmail.com>
2026-01-06 16:37:06 +0700

    Do creating records exercise lesson 5 node.js

--- create-a-pool-exercise/app.mjs
+++ create-a-pool-exercise/app.mjs
@@ -14,6 +14,36 @@ app.get("/movies", async (req, res) => {
 	});
 });
 
+app.post("/movies",async (req, res) => {
+	try{
+	const { title, description, genres, year, poster, rating } 
+	= req.body;
+
+	await pool.query(
+		`INSERT INTO movies (title, description, genres, year, poster, rating)
+		VALUES ($1, $2, $3, $4, $5, $6)`,
+		[
+			title,
+			description,
+			genres,
+			year,
+			poster,
+			rating,
+		]
+	);
+	return res.status(201).json(
+	{
+			message : "Movie has been created.",
+		}
+	);
+} catch (error) {
+	return res.status(500).json({
+		message: error.message,
+	})
+}
+});
+
+
 app.listen(port, () => {
 	console.log(`🚀 Server is running at ${port}`);
 });
