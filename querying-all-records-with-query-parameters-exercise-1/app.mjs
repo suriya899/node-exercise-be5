f67ecf98165e82f373c30d2affba5e4f373bac78
Suriya <ktaro112@gmail.com>
2026-01-09 18:16:48 +0700

    Do query-parameters-exercise-1

--- querying-all-records-with-query-parameters-exercise-1/app.mjs
+++ querying-all-records-with-query-parameters-exercise-1/app.mjs
@@ -7,14 +7,18 @@ const port = 4000;
 app.use(express.json());
 
 app.get("/movies", async (req, res) => {
+	let result
 	try {
 		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างล่างนี้ 🔽🔽🔽
-		const genresParam = null;
-		const result = await pool.query(
-			`
-    SELECT * FROM movies
-    `,
-			[]
+
+		//1. สร้าง api อ่านข้อมูล ด้วย query parameter
+		const genres = req.query.genres;
+		//2. เขียน query เพื่ออ่านข้อมูลด้วย connection pool
+		result = await pool.query(
+			`SELECT * FROM movies
+			WHERE 
+			(genres = $1 or $1 IS NULL OR $1 = '')`,
+			[genres] 
 		);
 		// แก้ไขโค้ดให้สามารถกรองผลลัพธ์ด้วย Parameter ได้ข้างบนนี้ 🔼🔼🔼
 
@@ -22,6 +26,7 @@ app.get("/movies", async (req, res) => {
 			data: result.rows,
 		});
 	} catch (e) {
+		console.error(e);
 		return res.json({
 			message: "ไม่สามารถเชื่อมต่อ Database ได้",
 		});
