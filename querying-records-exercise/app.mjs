0bee77dc5be7b12e305dc0a3467fabc77542d3d2
Suriya <ktaro112@gmail.com>
2026-01-09 14:54:10 +0700

    Do querying records exercise

--- querying-records-exercise/app.mjs
+++ querying-records-exercise/app.mjs
@@ -16,6 +16,27 @@ app.get("/movies", async (req, res) => {
 
 // 📍 **** สร้าง API เพื่อใช้ในการดูข้อมูลหนังแต่ละเรื่องด้วย movieId ตรงนี้ ****
 
+//1.เข้าถึงชุดข้อมูลที่จำเป็นต้องใช้ คือ movieId อยู่ใน endpoint parameter
+//2.เขียน query เพื่ออ่านข้อมูลโพสต์ด้วย connection pool
+//select * from post where movie_id=? ซึ่ง movieId มาจาก endpoint parameter
+//3.return response กลับไปหา client
+
+app.get("/movies/:movieId", async (req, res) =>{
+	try{
+const movieIdFromClient =  req.params.movieId;
+
+const results = await pool.query
+(`SELECT * FROM movies WHERE movie_id = $1`,[movieIdFromClient]);
+
+return res.status(200).json({
+	data: results.rows[0],
+});
+	}catch (error){
+		return res.status(500).json ({ message: "ไม่สามารถเชื่อมต่อ Database ได้" });
+}
+})
+
+
 app.listen(port, () => {
 	console.log(`🚀 Server is running at ${port}`);
 });
